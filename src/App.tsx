2880aee7323f92491d982e175cd7a7b674c0a8bc
Patryk Orsztynowicz <patrykorsztynowicz1997@gmail.com>
2022-07-28 14:09:46 +0100

    Added new title component, back button and fixed logic to only show dropdown when no person is selected

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { styled } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
-import { Box } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { friends, family } from "./constants/seating";
 import BasicSelect from "./components/Select";
 import { IPerson } from "./interfaces/person";
@@ -19,7 +19,7 @@ function App() {
   const names = [...friends, ...family];
 
   const [selectedPerson, setSelectedPerson] = React.useState({
-    name: "Imie/Name",
+    name: "",
     seat: "",
     plusOne: null,
   });
@@ -54,57 +54,68 @@ function App() {
   };
 
   const generateMessage = (person: IPerson) => {
-    const plusOne: IPerson | undefined = names.find(i => i.seat === person.plusOne);
+    const plusOne: IPerson | undefined = names.find(
+      (i) => i.seat === person.plusOne
+    );
     let plusOneDirection = "";
 
     if (plusOne) {
       const plusOneSeatNumber = parseInt(plusOne.seat.split("-")[1]);
       const personSeatNumber = parseInt(person.seat.split("-")[1]);
 
-      if (plusOneSeatNumber > personSeatNumber && plusOneSeatNumber % 2 !== 0) {
-        if (person.lang === "ENG")
-          plusOneDirection = "to your right";
-        else if (person.lang === "CZ")
-          plusOneDirection = "vpravo";
-        else
-          plusOneDirection = "po twojej prawej";
+      if (
+        (plusOneSeatNumber > personSeatNumber && plusOneSeatNumber % 2 !== 0) ||
+        (plusOneSeatNumber < personSeatNumber && plusOneSeatNumber % 2 === 0)
+      ) {
+        if (person.lang === "ENG") plusOneDirection = "to your right";
+        else if (person.lang === "CZ") plusOneDirection = "vpravo";
+        else plusOneDirection = "po twojej prawej";
+      } else {
+        if (person.lang === "ENG") plusOneDirection = "to your left";
+        else if (person.lang === "CZ") plusOneDirection = "vlevo";
+        else plusOneDirection = "po twojej lewej";
       }
-      else {
-        if (person.lang === "ENG")
-          plusOneDirection = "to your left";
-        else if (person.lang === "CZ")
-          plusOneDirection = "vlevo";
-        else
-          plusOneDirection = "po twojej lewej";
-      }
-    };
+    }
 
     if (person.lang === "ENG") {
       return (
-        <div style={{height: "350px", textAlign: "center"}}>
+        <div style={{ height: "320px", textAlign: "center", border: "1px solid black", marginBottom: "20px" }}>
           <h3>Hey, Thanks for coming!</h3>
-          <p>{`You are sitting at table #${person.seat} at seat #${person.seat}`}.</p>
-          {plusOne && <p>{`${plusOne.name} is sitting ${plusOneDirection}`}.</p>}
+          <p>
+            {`You are sitting at table #${person.seat} at seat #${person.seat}`}
+            .
+          </p>
+          {plusOne && (
+            <p>{`${plusOne.name} is sitting ${plusOneDirection}`}.</p>
+          )}
         </div>
       );
-    }
-    else if (person.lang === "CZ") {
+    } else if (person.lang === "CZ") {
       return (
-        <div style={{height: "350px"}}>
+        <div style={{ height: "320px", textAlign: "center", border: "1px solid black", marginBottom: "20px" }}>
           <h3>Ahoj, děkujeme za přijetí!</h3>
           <p>{`Sedíte na stolu #${person.seat} na sedadle #${person.seat}`}</p>
           {plusOne && <p>{`${plusOne.name} sedí ${plusOneDirection}`}</p>}
         </div>
       );
-    }
-    else {
+    } else {
       return (
-        <div style={{height: "350px"}}>
+        <div style={{ height: "320px", textAlign: "center", paddingTop: "20px", border: "1px solid black", marginBottom: "20px" }}>
           <h3>Hej, dziękujemy za przybycie!</h3>
           <p>{`Siedzisz przy stole #${person.seat} na miejscu #${person.seat}`}</p>
           {plusOne && <p>{`${plusOne.name} siedzi ${plusOneDirection}`}</p>}
         </div>
       );
+    }
+  };
+
+  const generateBackMessage = (person: IPerson) => {
+    if (person.lang === "ENG") {
+      return "Go Back";
+    } else if (person.lang === "CZ") {
+      return "Jít zpět";
+    } else {
+      return "Wróć";
     }
   };
 
@@ -117,61 +128,118 @@ function App() {
       <div
         style={{
           width: "100%",
-          marginTop: "30px",
-          marginBottom: "30px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
+          paddingBottom: "10px",
+          borderBottom: selectedPerson.name && "1px solid #b8b7b6",
         }}
       >
-        <BasicSelect
-          items={names}
-          value={selectedPerson}
-          onChange={setSelectedPerson}
-        />
+        <Typography
+          sx={{
+            fontFamily: "Parisienne",
+            fontSize: "50px",
+            display: "flex",
+            justifyContent: "center",
+            marginTop: selectedPerson.name ? 0 : "30%",
+          }}
+        >
+          Emilia & Patryk
+        </Typography>
+        <Typography
+          sx={{
+            fontFamily: "Parisienne",
+            fontSize: "25px",
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "10px",
+          }}
+        >
+          27.08.2022
+        </Typography>
       </div>
       <div
         style={{
           width: "100%",
+          marginTop: "15px",
+          paddingBottom: "15px",
           display: "flex",
-          justifyContent: "center",
+          justifyContent: !selectedPerson.name ? "center" : "start",
           alignItems: "center",
         }}
       >
-        <Box sx={{ width: "95%", display: "flex", gap: "15%" }}>
-          <div
-            style={{ display: "flex-column", width: "50%", marginTop: "auto", textAlign: "center" }}
+        {!selectedPerson.name ? (
+          <BasicSelect
+            items={names}
+            value={selectedPerson}
+            onChange={setSelectedPerson}
+          />
+        ) : (
+          <Button
+            variant="contained"
+            style={{ backgroundColor: "gold", color: "black", marginLeft: "10px", marginBottom: "5px"}}
+            onClick={() =>
+              setSelectedPerson({ name: "", seat: "", plusOne: null })
+            }
           >
-            {selectedPerson.seat !== "" && generateMessage(selectedPerson)}
-            <Grid
-              container
-              rowSpacing={1}
-              columnSpacing={{ xs: 1, sm: 2, md: 3, marginLeft: "10%" }}
-            >
-              {friends.map((item, index) => (
-                <Grid item key={index} xs={6}>
-                  <Item style={{ ...cellStyle(item) }}>{chooseText(item)}</Item>
-                </Grid>
-              ))}
-            </Grid>
-          </div>
-          <div
-            style={{ display: "flex-column", width: "50%", marginTop: "auto" }}
-          >
-            <Grid
-              container
-              rowSpacing={1}
-              columnSpacing={{ xs: 1, sm: 2, md: 3, marginLeft: "10%" }}
-            >
-              {family.map((item, index) => (
-                <Grid item key={index} xs={6}>
-                  <Item style={{ ...cellStyle(item) }}>{chooseText(item)}</Item>
-                </Grid>
-              ))}
-            </Grid>
-          </div>
-        </Box>
+            {generateBackMessage(selectedPerson)}
+          </Button>
+        )}
       </div>
+      {selectedPerson.name !== "" && (
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Box sx={{ width: "95%", display: "flex", gap: "15%" }}>
+            <div
+              style={{
+                display: "flex-column",
+                width: "50%",
+                marginTop: "auto",
+                textAlign: "center",
+              }}
+            >
+              {selectedPerson.seat !== "" && generateMessage(selectedPerson)}
+              <Grid
+                container
+                rowSpacing={1}
+                columnSpacing={{ xs: 1, sm: 2, md: 3, marginLeft: "10%" }}
+              >
+                {friends.map((item, index) => (
+                  <Grid item key={index} xs={6}>
+                    <Item style={{ ...cellStyle(item) }}>
+                      {chooseText(item)}
+                    </Item>
+                  </Grid>
+                ))}
+              </Grid>
+            </div>
+            <div
+              style={{
+                display: "flex-column",
+                width: "50%",
+                marginTop: "auto",
+              }}
+            >
+              <Grid
+                container
+                rowSpacing={1}
+                columnSpacing={{ xs: 1, sm: 2, md: 3, marginLeft: "10%" }}
+              >
+                {family.map((item, index) => (
+                  <Grid item key={index} xs={6}>
+                    <Item style={{ ...cellStyle(item) }}>
+                      {chooseText(item)}
+                    </Item>
+                  </Grid>
+                ))}
+              </Grid>
+            </div>
+          </Box>
+        </div>
+      )}
     </div>
   );
 }
